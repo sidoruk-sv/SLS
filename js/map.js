8b2f004f5d73cc3b9e82099aaa0362a09f9933ee
sidoruk_sv <sidoruksergey@gmail.com>
2015-05-14 17:18:13 +0300

    Initial commit

---
+++ js/map.js
@@ -0,0 +1,22 @@
+sMap = function (array, iterator) {
+  if (!array) return;
+  if (!iterator) return array;
+  var buffArray = [];
+
+  if (Object.prototype.toString.call(array) !== '[object Array]') {
+
+    for (var j in array) {
+      if (array.hasOwnProperty(j)) {
+        buffArray.push(iterator(array[j], j, array))
+      }
+    }
+
+  } else {
+
+    for (var i = 0; i < array.length; i++) {
+      buffArray[i] = iterator(array[i]);
+    }
+
+  }
+  return buffArray;
+};
