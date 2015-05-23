7c4df979b76b38a64075a6f9f884835b82392bf3
sidoruk_sv <sidoruksergey@gmail.com>
2015-05-23 18:45:07 +0300

    fix some tests (reject, size, reduce, reduceRight)

--- tests/reduce-right-tests.js
+++ tests/reduce-right-tests.js
@@ -1,4 +1,4 @@
 QUnit.test("sReduceRight tests", function (assert) {
 
-  assert.equal(sReduceRight([[0, 1], [2, 3], [4, 5]], function(a, b) { return a.concat(b); }, []), [4, 5, 2, 3, 0, 1], " array of arrays to array of elements");
+  assert.deepEqual(sReduceRight([[0, 1], [2, 3], [4, 5]], function(a, b) { return a.concat(b); }, []), [4, 5, 2, 3, 0, 1], " array of arrays to array of elements");
 });
--- tests/size-tests.js
+++ tests/size-tests.js
@@ -1,5 +1,5 @@
 QUnit.test("Size tests", function (assert) {
-  assert.equal(sSize([0, 1, 2, 3, 'asd', [11,2,3], {asd:'ds', add:'adsa'}]), 4, 'detect size array');
+  assert.equal(sSize([0, 1, 2, 3, 'asd', [11,2,3], {asd:'ds', add:'adsa'}]), 7, 'detect size array');
   assert.equal(sSize({a:'a', b:'b', c:'c'}), 3, 'detect size object');
   assert.equal(sSize([0, 0, 0, 0,0]), 5, 'detect size array');
 });
