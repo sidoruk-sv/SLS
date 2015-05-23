e34d5cfb9bad9e28d16f23caecb5c45b806fe601
sidoruk_sv <sidoruksergey@gmail.com>
2015-05-23 15:32:47 +0300

    add reduce

---
+++ js/src/reduce.js
@@ -0,0 +1,28 @@
+sReduce = function (list, iterator, memo){
+  if (!list) return;
+  if (!iterator) return;
+  var  num, result, collection = list, index = 0;
+
+  if (Object.prototype.toString.call(collection) !== '[object Array]') {
+    if(!memo){
+        memo = ''
+    }
+    var keys = Object.keys(collection)
+    while (keys[index]) {
+      memo = iterator(memo, collection[keys[index]], index, collection)
+      index = index + 1
+    }
+    return memo
+  } else {
+    if(!memo){
+        memo = 0
+    }
+
+    while (collection[index]){
+  	  memo = iterator(memo, collection[index], index, collection)
+  	  index = index + 1
+ 	  }
+ 	  return memo
+  }
+
+}
---
+++ tests/reduce-tests.js
@@ -0,0 +1,11 @@
+QUnit.test("Reduce tests", function (assert) {
+  assert.equal(sReduce([1, 2, 3, 4], function(memo, current){ return memo + current}, 200), 210, 'memo + current');
+  assert.equal(sReduce([1, 2, 3, 4], function(memo, current){ return memo * current}, 1), 24, 'memo * current');
+  assert.equal(sReduce({a:'a', b:'b', c:'c'}, function(memo, current){ return memo + current}, 1), '1abc', 'objects memo + current');
+  assert.equal(sReduce([1, 2, 3], function(memo, current) { return memo + current; }), 6, '[1,2,3] (memo + current) === [1,2,3] (memo*current)');
+  assert.deepEqual(sReduce([[1,2], [2, 3], [3,4,5,[6,7]]], function(memo, current) { return memo.concat(current); }), [1,2,3,4,5,[6,7]], '[1,2,3] (memo + current) === [1,2,3] (memo*current)');
+
+  assert.notOk(sReduce(), "test if no list provided");
+  assert.notOk(sReduce([1, 2, 3]), "test if no iterator function");
+  assert.notEqual(sReduce([1, 1, 1, 1], function(memo, current){ return memo * current}), 2, 'memo * current');
+});
